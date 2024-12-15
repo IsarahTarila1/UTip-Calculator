7b0336041b06cc9a37a92b153a8f5ca9aa951ba5
isarahtarila1 <isarahtarila@gmail.com>
2024-12-15 13:48:20 +0100

    Widget Documentation & Basics of Theming

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -32,18 +31,23 @@ class UTip extends StatelessWidget {
         title: const Text('UTip'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            padding: const EdgeInsets.all(8),
-            decoration: BoxDecoration(
-              color: Colors.deepPurple,
-                borderRadius: BorderRadius.circular(10.0)),
-              child: const Column(
-                children: [
-                  Text("Total Per Person"),
-                  Text("\$23.5"),
-                ],
-              )),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: const EdgeInsets.all(18),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.inversePrimary,
+                  borderRadius: BorderRadius.circular(10.0)),
+                child: Column(
+                  children: [
+                    Text("Total Per Person",
+                    style: Theme.of(context).textTheme.displayMedium,),
+                    Text("\$23.5"),
+                  ],
+                )),
+          ),
         ],
       ),
     );
